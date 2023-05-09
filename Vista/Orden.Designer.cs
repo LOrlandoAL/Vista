71459beadee37b73123b5d854141017ffd4ea438
Agustín Balcázar Reyes <98267769+Agustin-Balcazar@users.noreply.github.com>
2023-05-09 10:02:04 -0600

    Vista de Ordenes

---
+++ Vista/Orden.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace Vista
+{
+    partial class Orden
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvProduct = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProduct)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvProduct
+            // 
+            this.dgvProduct.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvProduct.Location = new System.Drawing.Point(12, 41);
+            this.dgvProduct.Name = "dgvProduct";
+            this.dgvProduct.Size = new System.Drawing.Size(776, 397);
+            this.dgvProduct.TabIndex = 0;
+            // 
+            // Orden
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvProduct);
+            this.Name = "Orden";
+            this.Text = "Orden";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProduct)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvProduct;
+    }
+}
---
+++ Vista/Orden.cs
@@ -0,0 +1,37 @@
+﻿using Datos;
+using Modelo;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Vista
+{
+    public partial class Orden : Form
+    {
+        public static List<Product> products;
+
+        public Orden()
+        {
+            InitializeComponent();
+            Conexion con = new Conexion();
+
+            products = new ProductDAO().GetOrders();
+
+            dgvProduct.DataSource = products;
+            dgvProduct.AllowUserToAddRows = false;
+            dgvProduct.AllowUserToDeleteRows = false;
+            dgvProduct.EditMode = DataGridViewEditMode.EditProgrammatically;
+
+            dgvProduct.Columns["ProductName"].HeaderText = "Producto";
+            dgvProduct.Columns["CompanyName"].HeaderText = "Proveedor";
+            //dgvProduct.Columns["Unidades"].HeaderText = "Unidades";
+
+        }
+    }
+}
